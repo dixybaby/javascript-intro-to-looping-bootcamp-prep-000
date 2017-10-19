c3b20400401d1062110b5829ce6465c3c679cc14
Barbara Dixon <babsnwosisi@yahoo.com>
2017-10-19 15:50:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,17 +1,11 @@
 function forLoop(array){
-	for (var i = 0; i < 25; i++)
+	for (var i = 0; i < 25; i++){
 		if (i === 1){
 			array.push("I am 1 strange loop.")
 			}
 		else {
 			array.push("I am ${i} strange loops.")
 }
-  return array
 }
-
-function whileLoop(n) {
-  let countdown = 100
-  while (100 > 0) {
-    console.log(100 > 0)
-  }
+  return array
 }
